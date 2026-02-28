711dfffc0be11e05b4aa90f198b2ee049ded24fc
Boris Kisov <djbobus@mail.ru>
2026-02-28 22:02:38 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { cloudflare } from "@cloudflare/vite-plugin"; // <-- named import
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [
     react(),
-    cloudflare({
-      dir: "dist",
-      injectFetch: true,
-    }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8787",
-        changeOrigin: true,
-      },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
     },
+  },
+  build: {
+    sourcemap: true,
+    chunkSizeWarningLimit: 2000,
   },
 });
